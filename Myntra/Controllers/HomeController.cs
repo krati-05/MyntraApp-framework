c09830fb7022b5dd4c37b32f1889e7f347c7b5bb
Krati Ramsingh Bais <kratib@cybage.com>
2025-04-08 11:29:04 +0530

    code added to new repository

---
+++ Myntra/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Myntra.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            //dev branch
+
+            //Change 1 in dev branch
+            //Change 2 in dev branch
+            //Change 3 in dev branch
+            //Change 4 in dev branch
+            //Change 5 in dev branch
+            //Change 6 in dev branch
+            //Change 7 in dev branch
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
